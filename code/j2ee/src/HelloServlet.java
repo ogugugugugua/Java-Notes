fb33f4ff48fcf0e29c0cdeb1c316a62124f78005
XieYuLin <xieyl_github@163.com>
2019-11-30 18:11:27 +0100

    servlet beginning

---
+++ code/j2ee/src/HelloServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        try{
+            response.getWriter().println("<h1>Polytech!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
