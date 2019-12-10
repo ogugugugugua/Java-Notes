76bafaf136f31b75403cd41eb229c32512b820de
XieYuLin <xieyl_github@163.com>
2019-12-10 22:16:56 +0100

    load-on-startup

--- code/j2ee/src/HelloServlet.java
+++ code/j2ee/src/HelloServlet.java
@@ -1,9 +1,13 @@
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
 public class HelloServlet extends HttpServlet {
+    public void init(ServletConfig servletConfig){
+        System.out.println("initialization of hello servlet");
+    }
     public void doGet(HttpServletRequest request, HttpServletResponse response){
         try{
             response.getWriter().println("<h1>Polytech!</h1>");
