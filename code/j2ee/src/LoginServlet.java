9c7f6bf70f7417b46d935e5fc8148fecfa42deb4
XieYuLin <xieyl_github@163.com>
2019-12-10 20:23:26 +0100

    servlet

--- code/j2ee/src/LoginServlet.java
+++ code/j2ee/src/LoginServlet.java
@@ -4,22 +4,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public LoginServlet(){
+        System.out.println("Instanced！！！！！！！！");
+    }
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding(String.valueOf(StandardCharsets.UTF_8));
+
         String name = request.getParameter("name");
+//        byte[] bytes = name.getBytes(StandardCharsets.ISO_8859_1);
+//        name = new String(bytes, StandardCharsets.UTF_8);
         String password = request.getParameter("password");
 
         System.out.println("name: "+name);
         System.out.println("password: "+password);
 
-        String htmlResponse = null;
+        String html = null;
         if("admin".equals(name) && "123".equals(password)){
-            htmlResponse = "<div style='color:green'>SUCCESS!</div>";
+            html = "<div style='color:green'>登录成功! Login réussi! </div>";
         }else {
-            htmlResponse = "<div style='color:red'>FAILED!</div>";
+            html = "<div style='color:red'>FAILED!</div>";
         }
+        response.setContentType("text/html; charset=UTF-8");
         PrintWriter printWriter = response.getWriter();
-        printWriter.println(htmlResponse);
+        printWriter.println(html);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+
     }
 }
