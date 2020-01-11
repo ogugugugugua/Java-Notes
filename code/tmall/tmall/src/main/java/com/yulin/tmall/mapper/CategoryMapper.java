3eb58369d99214f882ff1c7d85372d415fa4add7
XieYuLin <xieyl_github@163.com>
2020-01-11 15:12:06 +0100

    change gitignore

--- code/tmall/tmall/src/main/java/com/yulin/tmall/mapper/CategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yulin.tmall.mapper;
- 
-import com.yulin.tmall.pojo.Category;
- 
-import java.util.List;
- 
-public interface CategoryMapper {
-    List<Category> list();
-}
--- code/tmall/tmall/src/main/java/com/yulin/tmall/service/CategoryService.java
+++
@@ -1,6 +0,0 @@
-package com.yulin.tmall.service;
-import com.yulin.tmall.pojo.Category;
-import java.util.List;
-public interface CategoryService{
-    List<Category> list();
-}
