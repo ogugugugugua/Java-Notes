031dba6b312635f9ab9cc5d37d043c1cf2a2710b
XieYuLin <xieyl_github@163.com>
2020-02-02 19:40:19 +0100

    resultMap

---
+++ code/MyBatis/MyBatisEnvironmentSetup/mybatis-03/src/test/java/dao/UserMapperTest.java
@@ -0,0 +1,23 @@
+package dao;
+
+import com.yulin.dao.UserMapper;
+import com.yulin.pojo.User;
+import com.yulin.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserMapperTest {
+    @Test
+    public void getUserById(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = mapper.getUserById(1);
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+}
