aafd2fe5a7ca272ebe087c42484de33a5e394f39
XieYuLin <xieyl_github@163.com>
2020-02-02 18:28:28 +0100

    mybatis-config.xml with external db.properties file

---
+++ code/MyBatis/MyBatisEnvironmentSetup/mybatis-02/src/main/java/com/yulin/utils/MybatisUtils.java
@@ -0,0 +1,35 @@
+package com.yulin.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 从 SqlSessionFactory 中获取 SqlSession
+ */
+//sqlSessionFactory--->SessionFactory
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try{
+            //使用mybatis第一步、获取sqlSessionFactory对象
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //既然有了 SqlSessionFactory，顾名思义，我们就可以从中获得 SqlSession 的实例了。
+    // SqlSession 完全包含了面向数据库执行 SQL 命令所需的所有方法。
+    // 你可以通过 SqlSession 实例来直接执行已映射的 SQL 语句。
+    public static SqlSession getSqlSession(){
+        return sqlSessionFactory.openSession();
+    }
+
+}
---
+++ code/MyBatis/MyBatisEnvironmentSetup/mybatis-02/src/test/java/dao/UserMapperTest.java
@@ -0,0 +1,78 @@
+package dao;
+
+import com.yulin.dao.UserMapper;
+import com.yulin.pojo.User;
+import com.yulin.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserMapperTest {
+    @Test
+    public void getUserList(){
+
+//        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        // In order to do the test, first step: get SqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        // Second step: get the needed DAO
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.getUserList();
+
+        //Third step: we then see the result
+        for(User user:userList){
+            System.out.println(user);
+        }
+
+        //close sqlsession
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserById(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = mapper.getUserById(1);
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        mapper.updateUser(new User(4,"cfour","ftk98"));
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        mapper.insertUser(new User(5,"five","ffwjfio"));
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        mapper.deleteUser(5);
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+}
