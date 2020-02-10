23e74a02035d98777a2d17363fbe36466dc5e13e
XieYuLin <xieyl_github@163.com>
2020-02-10 01:35:51 +0100

    产品属性值管理

---
+++ code/tmall/tmall-产品属性值设置/src/main/java/com/yulin/tmall/controller/PropertyValueController.java
@@ -0,0 +1,39 @@
+package com.yulin.tmall.controller;
+
+import com.yulin.tmall.pojo.Product;
+import com.yulin.tmall.pojo.PropertyValue;
+import com.yulin.tmall.service.ProductService;
+import com.yulin.tmall.service.PropertyValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+public class PropertyValueController {
+    @Autowired
+    PropertyValueService propertyValueService;
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping("admin_propertyValue_edit")
+    public String edit(Model model,int pid) {
+        Product p = productService.get(pid);
+        propertyValueService.init(p);
+        List<PropertyValue> pvs = propertyValueService.list(p.getId());
+
+        model.addAttribute("p", p);
+        model.addAttribute("pvs", pvs);
+        return "admin/editPropertyValue";
+    }
+    @RequestMapping("admin_propertyValue_update")
+    @ResponseBody
+    public String update(PropertyValue pv) {
+        propertyValueService.update(pv);
+        return "success";
+    }
+}
---
+++ code/tmall/tmall-产品属性值设置/src/main/java/com/yulin/tmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,83 @@
+package com.yulin.tmall.service.impl;
+
+import com.yulin.tmall.mapper.ProductMapper;
+import com.yulin.tmall.pojo.Category;
+import com.yulin.tmall.pojo.Product;
+import com.yulin.tmall.pojo.ProductExample;
+import com.yulin.tmall.pojo.ProductImage;
+import com.yulin.tmall.service.CategoryService;
+import com.yulin.tmall.service.ProductImageService;
+import com.yulin.tmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    ProductMapper productMapper;
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductImageService productImageService;
+
+    @Override
+    public void add(Product product) {
+        productMapper.insert(product);
+    }
+
+    @Override
+    public void delete(int id) {
+        productMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Product product) {
+        productMapper.updateByPrimaryKeySelective(product);
+    }
+
+    @Override
+    public Product get(int id) {
+        Product product = productMapper.selectByPrimaryKey(id);
+        setCategory(product); //由于在这里已经设置了该product对应的Category，所以在controller那里就不再需要重新设置了
+        setFirstProductImage(product);
+        return product;
+    }
+
+    @Override
+    public void setFirstProductImage(Product p) {
+        List<ProductImage> pis = productImageService.list(p.getId(), ProductImageService.type_single);
+        if (!pis.isEmpty()) {
+            ProductImage pi = pis.get(0);
+            p.setFirstProductImage(pi);
+        }
+    }
+
+    public void setFirstProductImage(List<Product> ps) {
+        for (Product p : ps) {
+            setFirstProductImage(p);
+        }
+    }
+
+    @Override
+    public List list(int cid) {
+        ProductExample example = new ProductExample();
+        example.createCriteria().andCidEqualTo(cid);
+        example.setOrderByClause("id desc");
+        List result = productMapper.selectByExample(example);
+        setCategory(result);
+        setFirstProductImage(result);
+        return result;
+    }
+
+    public void setCategory(List<Product> ps){
+        for (Product p : ps)
+            setCategory(p);
+    }
+    public void setCategory(Product p){
+        int cid = p.getCid();
+        Category c = categoryService.getByID(cid);
+        p.setCategory(c);
+    }
+}
