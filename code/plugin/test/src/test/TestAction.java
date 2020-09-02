96884792c54bd2e52aa5d4f683db2f2d96e6890d
XieYuLin <xieyl_github@163.com>
2020-09-02 22:52:09 +0200

    global update

---
+++ code/plugin/test/src/test/TestAction.java
@@ -0,0 +1,36 @@
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+
+public class TestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+//        NotificationGroup notificationGroup = new NotificationGroup("testid", NotificationDisplayType.BALLOON, false);
+//        Notification notification = notificationGroup.createNotification("test notification", MessageType.INFO);
+//        Notifications.Bus.notify(notification);
+
+
+        //获取当前在操作的工程上下文
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+
+        //获取当前操作的类文件
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        //获取当前类文件的路径
+        String classPath = psiFile.getVirtualFile().getPath();
+        String title = "Hello World!";
+
+        //显示对话框
+        Messages.showMessageDialog(project, classPath, title, Messages.getInformationIcon());
+    }
+}
