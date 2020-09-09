da6ee2174d844741d6c8501d7357aba5f40db46f
XieYuLin <xieyl_github@163.com>
2020-09-09 22:23:54 +0200

    huawei

--- code/leetcode/StreamingExercisesLeetCode/src/string_sorting_1.java
+++ code/leetcode/StreamingExercisesLeetCode/src/string_sorting_1.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class string_sorting_1 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new LinkedList<>();
+
         try {
             int n = Integer.parseInt(br.readLine());
             String[] line = br.readLine().split(" ");
