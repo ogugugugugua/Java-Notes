ce058723f07bfa8b2e11919aef07b10582b0be5f
XieYuLin <xieyl_github@163.com>
2020-09-23 18:17:22 +0200

    寻找第k大

--- code/leetcode/寻找第K大/src/SolutionModified_1.java
+++ code/leetcode/寻找第K大/src/SolutionModified_1.java
@@ -1,5 +1,5 @@
 /**
- * 这个更容易理解，和普通的
+ * 这个更容易理解，和普通的快速排序有点像，当然由于k的存在，我们可以少排序一些数据
  */
 public class SolutionModified_1 {
     public static int sort(int[] a, int low, int high, int k){
@@ -20,7 +20,7 @@ public class SolutionModified_1 {
                 j--;
             }
         }
-        a[i] = index;   //其实在这里就可以知道：index的值处于数组的第i+1大。因为前面的第
+        a[i] = index;   //其实在这里就可以知道：index的值处于数组的第i+1大。因为前面的那些元素都小于它，后面的元素都大于它，虽然前后的子集都是无序的
         if(i<k-1){
             return sort(a, i+1, high, k);
         }else if(i>k-1){
