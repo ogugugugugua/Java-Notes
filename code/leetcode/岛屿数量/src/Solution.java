16d8f181cd495422ad17c1e9ec7bd2461f2c69cb
XieYuLin <xieyl_github@163.com>
2020-09-26 21:25:09 +0200

    岛屿数量 v2

--- code/leetcode/岛屿数量/src/Solution.java
+++ code/leetcode/岛屿数量/src/Solution.java
@@ -1,8 +1,7 @@
-//v1: 请检查是否存在数组越界等非法访问情况 用例通过率: 93.75% 运行时间: 130ms 占用内存: 21952KB
-//case通过率为93.75%
-//Exception in thread "main" java.lang.StackOverflowError
-//at Solution.setZero(Solution.java:15)
-//思路和代码都没错，本地运行成功，但是OJ的系统出现了StackOverflowError
+//v2
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
     public static void setZero(char[][] grid, int i, int j){
@@ -26,6 +25,7 @@ public class Solution {
         }
     }
     public static int solve (char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
         int res = 0;
         int lenX = grid.length;
         int lenY = grid[0].length;
@@ -33,7 +33,32 @@ public class Solution {
             for(int y = 0; y<lenY; y++){
                 if(grid[x][y]=='1'){
                     res++;
-                    setZero(grid, x, y);
+                    grid[x][y]= '0';                                //当前点清零
+                    Queue<Integer> queue = new LinkedList<>();      //使用队列能够避免进行递归操作
+                    //element = x*lenY+y ; x = element/lenY; y = element%lenY;
+                    queue.offer(x*lenY+y);
+                    while (!queue.isEmpty()) {
+                        int n = queue.poll();
+                        int i = n / lenY;
+                        int j = n % lenY;
+//                        grid[i][j] = '0';                         //清零的操作如果在这里进行就会导致重复地把同一个压进队列，导致运行时间暴增
+                        if (i > 0 && grid[i-1][j] == '1') {
+                            grid[i-1][j] = '0';                     //清零的操作应该直接在这里进行
+                            queue.add((i - 1) * lenY + j);
+                        }
+                        if (i < lenX - 1 && grid[i+1][j] == '1') {
+                            grid[i+1][j] = '0';
+                            queue.add((i + 1) * lenY + j);
+                        }
+                        if (j > 0 && grid[i][j-1] == '1') {
+                            grid[i][j-1] = '0';
+                            queue.add(i * lenY + j - 1);
+                        }
+                        if (j < lenY - 1 && grid[i][j+1] == '1') {
+                            grid[i][j+1] = '0';
+                            queue.add(i * lenY + j + 1);
+                        }
+                    }
                 }
             }
         }
@@ -41,7 +66,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        char[][] grid ={{1,1,0,0,0},{0,1,0,1,1},{0,0,0,1,1},{0,0,0,0,0},{0,0,1,1,1}};
+        char[][] grid ={{'1','1','0','0','0'},{'0','1','0','1','1'},{'0','0','0','1','1'},{'0','0','0','0','0'},{'0','0','1','1','1'}};
         System.out.println(solve(grid));
     }
 }
