f7573b7eea99781746169597098c9512a5073636
XieYuLin <xieyl_github@163.com>
2020-09-26 10:31:44 +0200

    求二叉树的层序遍历 注释update

--- code/leetcode/求二叉树的层序遍历/src/Solution.java
+++ code/leetcode/求二叉树的层序遍历/src/Solution.java
@@ -30,28 +30,28 @@ class TreeNode {
 public class Solution {
 
     public static ArrayList<ArrayList<Integer>> levelOrder (TreeNode root) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> layer = new ArrayList<>();
-        if(root == null) return new ArrayList<>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();  //结果集
+        ArrayList<Integer> layer = new ArrayList<>();           //用来放置每一层的元素
+        if(root == null) return new ArrayList<>();              //空直接返回
 
-        TreeNode temp;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        TreeNode temp;                      //用于存放队列弹出的元素
+        Queue<TreeNode> queue = new LinkedList<>();             //队列
+        queue.offer(root);                  //先将头结点放进去
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
+            int size = queue.size();        //在这里记录当前queue的大小非常重要，因为它会不断变化，就无法知道该层的元素个数
             while (size-- > 0) {
                 temp = queue.poll();
-                layer.add(temp.val);
+                layer.add(temp.val);        //弹出的这个元素属于当前层，可以直接放进结果
                 if (temp.left!=null){
-                    queue.offer(temp.left);
+                    queue.offer(temp.left); //左子树非空则进队列
                 }
                 if (temp.right != null) {
-                    queue.offer(temp.right);
+                    queue.offer(temp.right);//右子树非空则进队列
                 }
             }
-            res.add(layer);
-            layer = new ArrayList<>();
+            res.add(layer);                 //把当前层放进结果集
+            layer = new ArrayList<>();      //初始化当前层
         }
 
         return res;
@@ -59,7 +59,6 @@ public class Solution {
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(6), new TreeNode(7))), new TreeNode(3));
-//        TreeNode root = new TreeNode(1);
         ArrayList<ArrayList<Integer>> res = levelOrder(root);
         System.out.println(res);
     }
