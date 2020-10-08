4de20b45aa466433f39d05c4ba22c2385e8c0592
ogugugugugua <1143724821@qq.com>
2020-10-08 23:32:41 +0200

    全排列

--- code/leetcode/全排列/src/Solution.java
+++ code/leetcode/全排列/src/Solution.java
@@ -1,18 +1,41 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
-    Set<List<Integer>> set = new HashSet<>();
-    public void dfs(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
 
-        }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int len = nums.length;
+        if (len==0) return result;
+        boolean[] used = new boolean[len];
+        List<Integer> path = new ArrayList<>();
+        dfs(nums, len, 0, path, used, result);
+        return result;
     }
 
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        Solution solution = new Solution();
+        List<List<Integer>> lists = solution.permute(nums);
+        System.out.println(lists);
+    }
 
+
+    public void dfs(int[] nums, int len, int depth, List<Integer> path, boolean[] used, List<List<Integer>> result) {
+        if (depth == len) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        // 在非叶子结点处，产生不同的分支，这一操作的语义是：在还未选择的数中依次选择一个元素作为下一个位置的元素，这显然得通过一个循环实现。
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(nums, len, depth + 1, path, used, result);
+                // 注意：下面这两行代码发生 「回溯」，回溯发生在从 深层结点 回到 浅层结点 的过程，代码在形式上和递归之前是对称的
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
     }
 }
