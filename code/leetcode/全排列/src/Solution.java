11d4b0986d8798b715eaee2f72b464a30cf8c6d1
XieYuLin <xieyl_github@163.com>
2020-10-08 22:13:27 +0200

    全排列

---
+++ code/leetcode/全排列/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    Set<List<Integer>> set = new HashSet<>();
+    public void dfs(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+    }
+}
