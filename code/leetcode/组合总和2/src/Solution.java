d5c81d3e09926b97d0f1db39e40545eef4d2d0c5
ogugugugugua <1143724821@qq.com>
2020-10-11 19:00:42 +0200

    组合总和2

--- code/leetcode/组合总和2/src/Solution.java
+++ code/leetcode/组合总和2/src/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] candidates = new int[]{10,1,2,7,6,1,5};
+        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
         int target = 8;
         System.out.println(new Solution().combinationSum2(candidates, target));
     }
@@ -14,42 +14,33 @@ public class Solution {
         if (len == 0) return res;
         Arrays.sort(candidates);
         List<Integer> path = new ArrayList<>();
-        boolean[] used = new boolean[len];
         int sum = 0;
-        dfs(candidates, target, len, 0, sum, used, path, res);
+        dfs(candidates, target, len, sum, 0, path, res);
         return res;
     }
 
-    public void dfs(int[] candidates, int target, int len, int depth, int sum, boolean[] used, List<Integer> path, List<List<Integer>> res) {
-        if (depth > len)
-            return;
+    public void dfs(int[] candidates, int target, int len, int sum, int index, List<Integer> path, List<List<Integer>> res) {
         if (sum == target) {
             List<Integer> temp = new ArrayList<>(path);
             Collections.sort(temp);
-            if (!res.contains(temp))
-                res.add(temp);
+            res.add(temp);
             return;
         }
 
-        mark: for (int i = 0; i < len; i++) {
-            if (!used[i]) {
-                for (int j = 0; j < i; j++) {
-                    if (!used[j] && candidates[i] == candidates[j]) {
-                        continue mark;
-                    }
-                }
-                sum += candidates[i];
-                if (sum > target) {
-                    sum -= candidates[i];
-                    break;
-                }
-                used[i] = true;
-                path.add(candidates[i]);
-                dfs(candidates, target, len, depth + 1, sum, used, path, res);
+        for (int i = index; i < len; i++) {
+            //小剪枝：同一层相同数值的节点，从第2个开始候选数更少，结果一定发生重复，因此用continue跳过当前数
+            if (i > index && candidates[i] == candidates[i - 1])  //对于已经排序的有重复数组是可以这样操作的
+                continue;
+            sum += candidates[i];
+            //大剪枝：如果加上当前数就已经大于target，那对于后面的数更加如此，不必继续尝试，直接返回return
+            if (sum > target) {
                 sum -= candidates[i];
-                path.remove(path.size() - 1);
-                used[i] = false;
+                return;                                     //对于已经排序的升序数组是可以这样操作的
             }
+            path.add(candidates[i]);
+            dfs(candidates, target, len, sum, i + 1, path, res);  //递归调用
+            sum -= candidates[i];                           //回溯
+            path.remove(path.size() - 1);               //回溯
         }
     }
 }
