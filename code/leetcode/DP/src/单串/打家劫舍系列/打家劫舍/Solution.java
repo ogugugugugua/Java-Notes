7fbcb1a8133b2ae9a2943f1be386c1d84622c10b
ogugugugugua <1143724821@qq.com>
2020-10-26 12:48:16 +0100

    打家劫舍

---
+++ code/leetcode/DP/src/单串/打家劫舍系列/打家劫舍/Solution.java
@@ -0,0 +1,18 @@
+package 单串.打家劫舍系列.打家劫舍;
+
+public class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return nums[0];
+        if (len == 2) return Math.max(nums[0],nums[1]);
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[len-1];
+    }
+}
